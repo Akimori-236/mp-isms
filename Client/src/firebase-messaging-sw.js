cda483fafeb38fcec0f093d9d0ddfb7b1bbb95f8
Akimori <wee.bscl@gmail.com>
2023-05-20 15:25:07 +0800

    sw.js

---
+++ Client/src/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging.js"
+);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBhpm2-3x92nywW9vxqpGAj1qMsedVy4eQ",
+  authDomain: "tfip-project-382915.firebaseapp.com",
+  projectId: "tfip-project-382915",
+  storageBucket: "tfip-project-382915.appspot.com",
+  messagingSenderId: "869245493728",
+  appId: "1:869245493728:web:09a9dc750bdd8229988b85",
+  measurementId: "G-2LGFVP8ZK4",
+});
