0ce228ba8ca18e1d793d49fe01d6234a76805a75
Akimori <wee.bscl@gmail.com>
2023-05-20 15:37:06 +0800

    add messaging svc

---
+++ Client/src/app/services/messaging.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { AngularFireMessaging } from '@angular/fire/compat/messaging';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagingService {
+  currentMessage = new BehaviorSubject<any>(null)
+
+  constructor(private angularFireMessaging: AngularFireMessaging) { }
+
+  requestPermission() {
+    this.angularFireMessaging.requestToken.subscribe(
+      (token) => {
+        console.log(token)
+      },
+      (err) => {
+        console.warn("Unable to get permissino for push notification:", err)
+      }
+    )
+  }
+
+  receiveMessaging() {
+    this.angularFireMessaging.messages.subscribe(
+      (payload) => {
+        console.log("New message received", payload)
+        this.currentMessage.next(payload)
+      }
+    )
+  }
+
+}
