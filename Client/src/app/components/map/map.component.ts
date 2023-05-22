cda2cd46b8976758d63b74ae23c138db161e81d8
Akimori <wee.bscl@gmail.com>
2023-05-23 02:26:18 +0800

    map ok

--- Client/src/app/components/map/map.component.ts
+++ Client/src/app/components/map/map.component.ts
@@ -7,16 +7,28 @@ import { maps } from 'google-one-tap';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
   map!: google.maps.Map;
   lat = 1.3596865
   lng = 103.818
   mapOptions: google.maps.MapOptions = {
     center: { lat: this.lat, lng: this.lng },
-    zoom: 11,
+    zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  markerOptions: google.maps.MarkerOptions = {
+    position: { lat: this.lat, lng: this.lng },
+    animation: google.maps.Animation.BOUNCE
   }
 
   constructor(public activeModal: NgbActiveModal) { }
 
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        this.lat = success.coords.latitude
+        this.lng = success.coords.longitude
+      }
+    )
+  }
 }
