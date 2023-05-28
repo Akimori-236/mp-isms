aa5e1077701acef2c1cbde642160a8df76f15202
Akimori <wee.bscl@gmail.com>
2023-05-28 23:23:32 +0800

    fix Boolean vs boolean

--- Server/src/main/java/tfip/akimori/server/models/Instrument.java
+++ Server/src/main/java/tfip/akimori/server/models/Instrument.java
@@ -17,9 +17,21 @@ public class Instrument {
     private String serial_number;
     private String store_id;
     private String store_name;
-    private boolean isRepairing;
+    private Boolean isRepairing;
     private String email;
     private String givenname;
     private String familyname;
     private String remarks;
+
+    public Boolean isRepairing() {
+        return this.isRepairing;
+    }
+
+    public void setIsRepairing(Boolean isRepairing) {
+        this.isRepairing = isRepairing;
+    }
+
+    public void setIsRepairing(boolean isRepairing) {
+        this.isRepairing = isRepairing;
+    }
 }
